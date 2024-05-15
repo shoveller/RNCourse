a7e790aeb06ed09659e95c240108e36e1ff230c2
cinos81 <cinos81@gmail.com>
2024-05-15 22:29:53 +0900

    section 1-8

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,9 +3,9 @@ import tw from 'twrnc'
 
 export default function HomeScreen() {
     return (
-        <View style={tw`flex-1 bg-white flex items-center justify-center`}>
+        <View style={tw`flex-1 bg-white items-center justify-center`}>
             <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar />
+            <StatusBar barStyle="dark-content" />
         </View>
     );
 }
