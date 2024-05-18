fb3d65a972e844a461574189e80655e476d75eff
cinos81 <cinos81@gmail.com>
2024-05-18 14:31:06 +0900

    apply biome

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
-import {Text, View, StatusBar} from 'react-native';
+import {Button, Text, TextInput, View} from 'react-native';
 import tw from 'twrnc'
 
 export default function HomeScreen() {
     return (
-        <View style={tw`flex-1 bg-white items-center justify-center`}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar barStyle="dark-content" />
+        <View style={tw`flex-1 p-[50px]`}>
+            <View style={tw`bg-pink-200`}>
+                <TextInput placeholder="당신의 목표!"/>
+                <Button title="목표 추가하기"/>
+            </View>
+            <View style={tw`bg-green-100`}>
+                <Text>목표가 들어갈 곳</Text>
+            </View>
         </View>
     );
 }
