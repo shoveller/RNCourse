bc88d2ca24e444529ec89230ace02b72960e1614
cinos81 <cinos81@gmail.com>
2024-05-19 18:08:33 +0900

    상태표시줄 제어 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,24 @@
-import {View, Button} from 'react-native';
+import {Button, View} from 'react-native';
 import tw from 'twrnc';
 import InputSection from "@/components/main/InputSection";
 import OutputSection from "@/components/main/OutputSection";
 import {useSetAtom} from "jotai";
 import {modalAtom} from "@/components/main/store";
+import {StatusBar} from 'expo-status-bar'
 
 export default function HomeScreen() {
     const open = useSetAtom(modalAtom);
 
     return (
-        <View style={tw`flex-1 p-[1rem] gap-[24px]`}>
-            <View style={tw`flex-1 justify-center items-center`}>
-                <Button title="새로운 목표 추가하기" color="red" onPress={() => open(true)} />
+        <>
+            <StatusBar style="auto" animated networkActivityIndicatorVisible backgroundColor="red" />
+            <View style={tw`flex-1 p-[1rem] gap-[24px]`}>
+                <View style={tw`flex-1 justify-center items-center`}>
+                    <Button title="새로운 목표 추가하기" color="red" onPress={() => open(true)}/>
+                </View>
+                <InputSection/>
+                <OutputSection/>
             </View>
-            <InputSection/>
-            <OutputSection/>
-        </View>
+        </>
     );
 }
